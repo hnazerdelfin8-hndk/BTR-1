12f211a83b3ff624088c53b342bbe2ff2570bc51
Sixhundredgoons <hnazerdelfin8@gmail.com>
2026-09-02 08:57:36 +0800

    Register all BTR 1 skills

--- skills/index.js
+++ skills/index.js
@@ -2,9 +2,15 @@ import { registerSkill } from '../core/router/index.js';
 import { createReminder } from './reminder.js';
 import { createNote } from './notes.js';
 import { createAutomation } from './automation.js';
+import { weather } from './weather.js';
+import { search } from './search.js';
+import { music } from './music.js';
 
 export function registerDefaultSkills() {
+  registerSkill('weather', weather);
+  registerSkill('search', search);
   registerSkill('reminder', createReminder);
   registerSkill('notes', createNote);
+  registerSkill('music', music);
   registerSkill('automation', createAutomation);
 }
